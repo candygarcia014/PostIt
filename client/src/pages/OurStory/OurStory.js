bda04c98faa15476cb8d5bd527056569e5194064
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-04 14:51:16 -0800

    changed font colors

--- client/src/pages/OurStory/OurStory.js
+++ client/src/pages/OurStory/OurStory.js
@@ -5,18 +5,13 @@ import { Container, Row, Col } from 'react-bootstrap';
 import DevImage from './../../components/DevImage/DevImage';
 import developers from '../../developers.json'
 
-// class OurStory extends Component {
-//   // Setting this.state.friends to the friends json array
-//   state = {
-//     developers
-//   };
 
 const OurStory = (props) => {
   return (
   <Container>
     
     <div>
-      <h1>Our Story</h1>
+      <h1 style={{color: "#c4c4c4"}}>Our Story</h1>
     </div>
 
     {/* Card for Our Story */}
@@ -27,7 +22,7 @@ const OurStory = (props) => {
     </Row>
 
     <div>
-      <h1 style={{width: "33%"}}>Meet the Developers!</h1>
+      <h1 style={{width: "33%", color: "#c4c4c4"}}>Meet the Developers!</h1>
     </div>
 
 
