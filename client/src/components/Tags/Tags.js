3737f840ee6c14d4cd57bcee182a7111a7d8af6b
ttngu <tammyn137@gmail.com>
2021-02-04 19:03:40 -0800

    added tag dropdown and styling

--- client/src/components/Tags/Tags.js
+++ client/src/components/Tags/Tags.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import {DropdownButton, Dropdown} from 'react-bootstrap';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import '../Tags/Tags.css'
 
@@ -10,14 +11,23 @@ function Tags() {
     const handleChange = (val) => setValue(val);
   
     return (
-      <ToggleButtonGroup size='sm' vertical={true} type="checkbox" value={value} onChange={handleChange}>
-        <ToggleButton className="tag-spacing" value={1}>SKIN</ToggleButton>
-        <ToggleButton className="tag-spacing" value={2}>HAIR</ToggleButton>
-        <ToggleButton className="tag-spacing" value={3}>BODY</ToggleButton>
-        <ToggleButton className="tag-spacing" value={4}>MIND</ToggleButton>
-        <ToggleButton className="tag-spacing" value={5}>STYLE</ToggleButton>
-        <ToggleButton className="tag-spacing" value={6}>NAILS</ToggleButton>
-      </ToggleButtonGroup>     
+      // <ToggleButtonGroup size='sm' vertical={true} type="checkbox" value={value} onChange={handleChange}>
+      //   <ToggleButton className="tag-spacing" value={1}>SKIN</ToggleButton>
+      //   <ToggleButton className="tag-spacing" value={2}>HAIR</ToggleButton>
+      //   <ToggleButton className="tag-spacing" value={3}>BODY</ToggleButton>
+      //   <ToggleButton className="tag-spacing" value={4}>MIND</ToggleButton>
+      //   <ToggleButton className="tag-spacing" value={5}>STYLE</ToggleButton>
+      //   <ToggleButton className="tag-spacing" value={6}>NAILS</ToggleButton>
+      // </ToggleButtonGroup>     
+
+      <DropdownButton id="dropdown-basic-button" title="FILTER" >
+      <Dropdown.Item href="#/action-1">SKIN</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">HAIR</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">BODY</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">MIND</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">STYLE</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">NAILS</Dropdown.Item>
+      </DropdownButton>
     );
   }
 
