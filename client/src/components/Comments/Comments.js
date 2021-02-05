555dcd0da887c99ee3630ee0d6fe36795dadcafa
ttngu <tammyn137@gmail.com>
2021-02-04 18:37:41 -0800

    updated card body styling

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -6,7 +6,7 @@ import './Comments.css';
 const Comments = (props) => {
 
     return (
-    <Card className="p-0 my-2">
+    <Card className="p-0 my-2" id="CommentBody">
         <Card.Body>
             <Card.Subtitle 
                 className="mb-2 text-muted meta-data-date-time meta-data">
