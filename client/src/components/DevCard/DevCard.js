862c376fc195bf8ef409b9e3ff94b7eb61ea8ed3
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-08 11:20:37 -0800

    added rotate on hover for dev info button

--- client/src/components/DevCard/DevCard.js
+++ client/src/components/DevCard/DevCard.js
@@ -8,12 +8,12 @@ import '../../developers.json'
 
 const DevCard = (props) => {
     return (
-
+        
         <Card className="dev-card" style={{maxHeight: "20rem !important", maxWidth: "50rem" }}>
         
             <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
-                {/* <BsFillPlusSquareFill /> */}
+                {/* <Card.Title>{props.name}</Card.Title> */}
+                   
                     <Card.Text>
                        <a href={props.github}><img className="github" src="https://github.com/ShanniSnax/self-care-pirates/blob/main/client/src/components/images/github-icon.png?raw=true"/></a> 
                     </Card.Text>
--- client/src/components/DevInfoButton/DevInfoButton.js
+++ client/src/components/DevInfoButton/DevInfoButton.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { BsFillPlusSquareFill } from 'react-icons/fa';
-import "./DevInfoButton.css"
+import "./DevInfoButton.css";
+import DevCard from './../DevCard/DevCard'; 
 
 
-const DevInfoButton = () => {
-   return (
+
+class DevInfoButton extends React.Component {
+    state = { showing: true };
     
-        <div className="plus-sign">
-            <i className="fa fa-arrow-up"></i>
-        </div>
-    
-)};
+        render() {
+        let { showing } = this.state;
+            return (
+            <button className="fa fa-plus-square" onClick={() => this.setState({ showing: !showing })}>
+            { !showing 
+                    ? <DevCard 
+                    name={this.props.name}
+                    github={this.props.github}
+                    linkedin={this.props.linkedin} />
+                    : null
+                }
+            </button>
+      )
+    }
+  }
 
 
+  
 
 export default DevInfoButton;
