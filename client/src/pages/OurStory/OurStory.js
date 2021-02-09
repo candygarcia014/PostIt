8c6656361e2bcff53e53e6ab2979c7460515d436
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-09 01:12:26 -0800

    fixed positioning for devcard and dev names

--- client/src/pages/OurStory/OurStory.js
+++ client/src/pages/OurStory/OurStory.js
@@ -9,42 +9,49 @@ import DevInfoButton from '../../components/DevInfoButton/DevInfoButton';
 
 const OurStory = (props) => {
   return (
-  <Container>
-    
-    <div>
-      <h1 style={{color: "#c4c4c4"}}>Our Story</h1>
-    </div>
+    <Container>
 
-    {/* Card for Our Story */}
-    <Row>
-      <Col xs={8} md={8}>
-        <StoryCard />
-      </Col>
-    </Row>
+      <div>
+        <h1 style={{ color: "#c4c4c4" }}>Our Story</h1>
+      </div>
 
-    <div>
-      <h1 style={{width: "33%", color: "#c4c4c4"}}>Meet the Developers!</h1>
-    </div>
+      {/* Card for Our Story */}
+      <Row>
+        <Col xs={8} md={8}>
+          <StoryCard />
+        </Col>
+      </Row>
+
+      <div>
+        <h1 style={{ width: "33%", color: "#c4c4c4" }}>Meet the Developers!</h1>
+      </div>
 
 
-    {developers.map((dev, i) => (
-          <Row className={i % 2 === 0 ? "" : "d-flex flex-row-reverse"}>
+      {developers.map((dev, i) => (
+        <Row className={i % 2 === 0 ? "" : "d-flex flex-row-reverse"} style={{height:'15rem'}}>
           <Col xs={3}>
             <DevImage source={dev.image} className={i % 2 === 0 ? "dev-image-left" : "dev-image-right"} />
-          </Col>
-          
-          <Col xs={9}  className="my-auto">
-            <h1 style={{width: "20%", color: "#c4c4c4"}}>{dev.name}</h1>
-            <DevInfoButton 
+            <div>
+              {dev.name}<DevInfoButton
               name={dev.name}
               github={dev.github}
-              linkedin={dev.linkedin} 
-            />
-            
+              linkedin={dev.linkedin}/>
+            </div>
+          </Col>
+
+          <Col xs={9} className="my-auto">
+            {/* <h1 className="dev-name" style={{ width: "20%", color: "#c4c4c4" }}>{dev.name}
+              <DevInfoButton
+                name={dev.name}
+                github={dev.github}
+                linkedin={dev.linkedin}
+              />
+            </h1> */}
+
           </Col>
         </Row>
-    ))}
-  </Container>
+      ))}
+    </Container>
 
   );
 };
