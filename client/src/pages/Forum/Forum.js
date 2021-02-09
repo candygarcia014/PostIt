f3afb6a00ef81c29ab0182e5c897a3284db49dcb
ttngu <tammyn137@gmail.com>
2021-02-08 22:39:38 -0800

    removed fake data comments from shanni

--- client/src/pages/Forum/Forum.js
+++ client/src/pages/Forum/Forum.js
@@ -11,51 +11,6 @@ import Tags from "../../components/Tags/Tags";
 // import CommentModal from '../CommentModal/CommentModal.js';
 import CreatePostModal from "../../components/CreatePostModal/CreatePostModal.js";
 
-//fake data placeholders
-
-// const fakeData = [
-//   {
-//     date: 'October 10, 1992',
-//     time: '10:30pm',
-//     username: 'shannisnax',
-//     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur accusantium aperiam ut! Voluptate et accusantium natus a omnis at odio fuga perferendis nulla similique recusandae dolor dolore placeat aspernatur nostrum, quisquam molestiae repellat, nemo laboriosam. Reprehenderit debitis hic dolorum, possimus veniam eligendi, optio saepe reiciendis sed exercitationem inventore voluptas quas sequi ipsa dolor excepturi est aliquam odio ex enim repudiandae eius quos? Iste nemo cum harum vitae aspernatur perferendis maiores minus esse quidem, recusandae saepe repellat. Molestiae quos doloribus ad natus nemo expedita quidem necessitatibus aperiam sed reprehenderit laboriosam odit rem iure eveniet commodi dolores tempore, distinctio odio corporis minus eligendi nostrum corrupti! Non nulla placeat dicta cumque ea velit sed? Reiciendis quidem corrupti, ratione sed aspernatur inventore deserunt voluptates dolore nulla ea quo tempora amet modi possimus ipsa nemo blanditiis omnis? Ipsum dolores autem dolore aperiam voluptate nesciunt eveniet, earum commodi rem quasi. Distinctio, maiores dolore sit quo cum quos ex nobis repellendus ea laboriosam necessitatibus atque libero eaque voluptatibus vitae cupiditate officiis assumenda expedita deleniti saepe doloremque quaerat asperiores incidunt. Saepe, et quaerat. Ea cupiditate vel quisquam fuga repellendus accusamus. Voluptatibus iste similique adipisci sint aperiam illo quam ratione voluptas sapiente perspiciatis, cum nesciunt, quia facilis quidem aut.",
-//     title: 'OMG Skin Care',
-//     id: 1
-//   },
-//   {
-//     date: 'January 3, 2020',
-//     time: '9:00am',
-//     username: 'kajshdsa',
-//     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur accusantium aperiam ut! Voluptate et accusantium natus a omnis at odio fuga perferendis nulla similique recusandae dolor dolore placeat aspernatur nostrum, quisquam molestiae repellat, nemo laboriosam. Reprehenderit debitis hic dolorum, possimus veniam eligendi, optio saepe reiciendis sed exercitationem inventore voluptas quas sequi ipsa dolor excepturi est aliquam odio ex enim repudiandae eius quos? Iste nemo cum harum vitae aspernatur perferendis maiores minus esse quidem, recusandae saepe repellat. Molestiae quos doloribus ad natus nemo expedita quidem necessitatibus aperiam sed reprehenderit laboriosam odit rem iure eveniet commodi dolores tempore, distinctio odio corporis minus eligendi nostrum corrupti! Non nulla placeat dicta cumque ea velit sed? Reiciendis quidem corrupti, ratione sed aspernatur inventore deserunt voluptates dolore nulla ea quo tempora amet modi possimus ipsa nemo blanditiis omnis? Ipsum dolores autem dolore aperiam voluptate nesciunt eveniet, earum commodi rem quasi. Distinctio, maiores dolore sit quo cum quos ex nobis repellendus ea laboriosam necessitatibus atque libero eaque voluptatibus vitae cupiditate officiis assumenda expedita deleniti saepe doloremque quaerat asperiores incidunt. Saepe, et quaerat. Ea cupiditate vel quisquam fuga repellendus accusamus. Voluptatibus iste similique adipisci sint aperiam illo quam ratione voluptas sapiente perspiciatis, cum nesciunt, quia facilis quidem aut.",
-//     title: 'What in the world is eye cream?',
-//     id: 2
-//   },
-//   {
-//     date: 'January 3, 2020',
-//     time: '9:00am',
-//     username: 'kajshdsa',
-//     body: "sdhkjhsadsahasdjsakldjsaljdsa",
-//     title: 'Face wash yay or nay?',
-//     id: 3
-//   },
-//   {
-//     date: 'January 3, 2020',
-//     time: '9:00am',
-//     username: 'kajshdsa',
-//     body: "sdhkjhsadsahasdjsakldjsaljdsa",
-//     title: 'Who uses manscape products?',
-//     id: 4
-//   },
-//   {
-//     date: 'January 3, 2020',
-//     time: '9:00am',
-//     username: 'kajshdsa',
-//     body: "sdhkjhsadsahasdjsakldjsaljdsa",
-//     title: 'Found the best beard oil ever wow!',
-//     id: 5
-//   }
-// ]
-
 const Forum = () => {
   const [data, setData] = useState();
   const [username, setUsername] = useState(JSON.parse(localStorage.getItem('username')));
